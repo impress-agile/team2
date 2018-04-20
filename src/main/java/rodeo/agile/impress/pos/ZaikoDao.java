f5e008e08df3ea1f07e2e6a4f1c9199820ba8a81
hiroyuki-s <h.sawaki@next-us.co.jp>
2018-04-20 20:55:41 +0900

    implements zaiko process

--- src/main/java/rodeo/agile/impress/pos/ZaikoDao.java
+++ src/main/java/rodeo/agile/impress/pos/ZaikoDao.java
@@ -1,10 +1,60 @@
 package rodeo.agile.impress.pos;
 
-public class ZaikoDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-	public void insert(int i, int j) {
-		// TODO Auto-generated method stub
-		
+public class ZaikoDao {
+	
+	String dbPath;
+
+	public ZaikoDao(String path) {
+		this.dbPath = path;
+	}
+
+	public void insert(int id, int suryo) throws ClassNotFoundException, SQLException {
+		Class.forName("org.sqlite.JDBC");
+		Connection connection = null;
+		Statement statement = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:sqlite:" + this.dbPath);
+			statement = connection.createStatement();
+			statement.setQueryTimeout(30);
+			try(ResultSet rs = statement.executeQuery("SELECT 1 FROM items WHERE id = " + id + ";");)
+			{
+				if(!rs.next())
+					throw new RuntimeException("items id = " + id + " not exist");
+			}
+			
+			
+			try(ResultSet rs = statement.executeQuery("SELECT 1 FROM zaiko WHERE id = " + id + ";");)
+			{
+				if(!rs.next())
+					statement.execute("INSERT INTO zaiko (id, suryo) VALUES (" + id + ", " + suryo + ");");
+				else
+					statement.execute("UPDATE zaiko SET suryo = suryo + " + suryo + " WHERE id = " + id + ";");
+			}
+			
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			try {
+				if (statement != null) {
+					statement.close();
+				}
+			} catch (SQLException e) {
+				throw e;
+			}
+			try {
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				throw e;
+			}
+		}
 	}
 
 }
--- src/main/java/rodeo/agile/impress/pos/ZaikoLogic.java
+++ src/main/java/rodeo/agile/impress/pos/ZaikoLogic.java
@@ -1,14 +1,20 @@
 package rodeo.agile.impress.pos;
 
+import java.sql.SQLException;
+
 public class ZaikoLogic {
+	
+	private ZaikoDao dao;
 
 	public ZaikoLogic(ZaikoDao dao) {
-		// TODO Auto-generated constructor stub
+		this.dao = dao;
 	}
-
-	public void add(int i, int j) {
-		// TODO Auto-generated method stub
 		
+	public void add(int id, int suryo) throws ClassNotFoundException, SQLException {
+		if (suryo <= 0) {
+			throw new RuntimeException("Failed to add zaiko which suryo lesser equal 0.");
+		}
+		dao.insert(id, suryo);
 	}
 
 }
